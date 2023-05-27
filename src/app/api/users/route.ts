f3ce4aec725d9bfe94a562959bdf0e47b742fb57
Charlie Charboneau <118317979+CharlieCharboneau@users.noreply.github.com>
2023-05-27 13:17:21 -0400

    database connection setup

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,22 @@
+// import DB connection
+import { db } from '../../../utils/database';
+
+
+export async function POST(request: Request) {
+    
+    try {
+        // const { username, password } = request.body;     // note from nestor: i don't think you can use request.body in nextjs13 anymore...https://www.youtube.com/watch?v=vrR4MlB7nBI potential solution: const body = await req.json()
+        const body = await request.json()
+        console.log(body)
+        
+        const queryStr = 'INSERT INTO users (username, password) VALUES ($1, $2);';
+
+        const newUser = await db.query(queryStr, ['test', 'test']);
+
+        console.log('this is the newUser -> ', newUser);
+
+    } catch(error) {
+        console.log(error)
+    }
+
+}
