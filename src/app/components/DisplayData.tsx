56879ac37860b17394c9aa396f8c2406b078efef
Matt Mattox <bizofmatt@gmail.com>
2023-06-02 14:28:13 -0700

    removed unnecessary code

--- src/app/components/DisplayData.tsx
+++ src/app/components/DisplayData.tsx
@@ -39,38 +39,3 @@ export function DisplayData(props: any) {
   );
 }
 
-// export function DisplayData(props: any) { //TODO: type
-//   console.log('data from DisplayData comp', props.data.schema);
-//   const schema = props.data.schema
-//   if (!schema) {
-//     return null; // or render an error message, loading state, or fallback UI
-//   }
-
-//   const logType = (type: any): void  => {
-//     console.log('this is the type', type);
-//   };
-
-//   return (
-//     <>
-//       <div className="m-4 p-4 border-2 border-red-600">
-//     <div>
-//       {Object.keys(schema).map((type) => (
-//         <div key={type}>
-//           {/* We can make this something different then an h3 if we want */}
-//           <h3>{type}:</h3>
-//             <ul>
-//               {schema[type].map((type: any, index: any) => (
-//                 <div key={index}>
-//                   <QueryCard
-//                   type={type}
-//                   />
-//                 </div>
-//             ))}
-//           </ul>
-//         </div>
-//           ))}
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
