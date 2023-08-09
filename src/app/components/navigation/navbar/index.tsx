607e2724675e5b5f546f900b68c72ac46ff391c8
Matt Mattox <bizofmatt@gmail.com>
2023-08-08 19:47:38 -0700

    removed comments

--- src/app/components/navigation/navbar/index.tsx
+++ src/app/components/navigation/navbar/index.tsx
@@ -43,36 +43,3 @@ const Navbar = () => {
     };
 
 export default Navbar
-
-
-{/* <div className="w-full h-20 bg-secondary sticky top-0 z-10">
-  <div className="container mx-auto px-4 h-full">
-    <div className="flex justify-between items-center h-full">
-      <Link href="/" className="ml-[-20px]"> 
-        <Logo />
-      </Link>
-      <ul className="hidden md:flex gap-x-6 text-white justify-end"> 
-       <li>
-                    <Link href="/signup">
-                      <p>Create Account</p>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/services">
-                      <p>About</p>
-                    </Link>
-                  </li>   
-                  <li>
-                    <Link href="/about">
-                      <p>Contributors</p>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/login">
-                      <p>Sign in</p>
-                    </Link>
-                  </li>
-      </ul>
-    </div>
-  </div>
-</div> */}
