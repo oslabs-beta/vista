d9bc3843940c2eb8edf4d894c06ee3df8007afac
Lucas Contreras <32287834+lucascontreras@users.noreply.github.com>
2023-08-10 18:30:06 -0400

    added warning message on objectnode

---
+++ src/app/components/ObjectNode.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { Node, NodeToolbar } from 'reactflow';
+
+export default function ObjectNode(props: Node) {
+  const [isVisible, setVisible] = useState(false);
+  return (
+    <div onMouseEnter={() => setVisible(true)} onMouseLeave={() => setVisible(false)}>  
+      <NodeToolbar
+        isVisible={isVisible}
+        offset={-10}
+        style={{
+          backgroundColor: 'lightyellow',
+          borderColor: 'black',
+          borderWidth: 1,
+          fontSize: '14px',
+          maxWidth: '200px',
+          textAlign: 'center',
+        }}
+      >
+        <p className="italic">Queries with a depth level greater than 2 are currently not supported.</p>
+      </NodeToolbar>
+      <div className="p-2 rounded-sm w-40 text-xs text-gray-500 text-center border border-solid border-gray-900 bg-white">
+        {props.data.label}
+      </div>
+    </div>
+  );
+};
