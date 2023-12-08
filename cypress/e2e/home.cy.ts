7162ce896ca30e3c08db3f115799dd6306755c0e
Lucas Contreras <32287834+lucascontreras@users.noreply.github.com>
2023-12-08 14:42:32 -0300

    created login command

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,7 @@
 const apiUrl = 'https://rickandmortyapi.com/graphql';
 
-// This code will run once before any tests in the describe block
+
+// This code will run once before each test in the describe block
 beforeEach(() => {
   cy.visit('/').then(() => {
     cy.get('input[name="endpoint-url"]').type(apiUrl);
@@ -9,8 +10,9 @@ beforeEach(() => {
 });
 
 describe('e2e Test for Vista', () => {
- 
-  xit('Should successfully log in the user to the application using their own credentials', () => {
+  
+  // Have user successfully sign in with their own credentials
+  it('Should successfully log in the user to the application using their own credentials', () => {
     cy.get('[data-testid="account-button"]').should('exist');
     cy.get('[data-testid="account-button"]').click();
     cy.get('[data-testid="sign-in-button"]').should('exist');
@@ -22,7 +24,7 @@ describe('e2e Test for Vista', () => {
     cy.contains('Sign in with Credentials').click()
     cy.get('[data-testid="endpoint-form"]').should('exist');
   })
-
+  
   it('Should render a root and fields node on the DOM', () => {
     // Check to see that the nodes with the data-testid attributes have rendered
     cy.get('.react-flow').should('exist');
@@ -41,20 +43,13 @@ describe('e2e Test for Vista', () => {
     cy.contains('query { characters { info } }').should('exist');
   });
 
-  // Have user successfully sign in and sign out with their own credentials
-  xit('Should successfully log in the user to the application using their own credentials', () => {
-    cy.get('[data-testid="account-button"]').should('exist');
-    cy.get('[data-testid="account-button"]').click();
-    cy.get('[data-testid="sign-in-button"]').should('exist');
-    cy.get('[data-testid="sign-in-button"]').click();
-    cy.get('[data-testid="username-input"]').should('exist');
-    cy.get('[data-testid="username-input"]').type('steph@dangernoodles.com');
-    cy.get('[data-testid="password-input"]').should('exist');
-    cy.get('[data-testid="password-input"]').type('123456789')
-    cy.contains('Sign in with Credentials').click()
-    cy.get('[data-testid="endpoint-form"]').should('exist');
-  })
+  
+  // Have user successfully sign out
   xit('Should successfully log out the user to the application using their own credentials', () => {
+    // custom command
+    cy.login('steph@dangernoodles.com','123456789');
+
+    // logout
     cy.get('[data-testid="account-button"]').should('exist');
     cy.get('[data-testid="account-button"]').click();
     cy.get('[data-testid="sign-out-button"]').should('exist');
@@ -62,6 +57,7 @@ describe('e2e Test for Vista', () => {
     cy.get('[data-testid="account-button"]').should('exist');
     cy.get('[data-testid="account-button"]').click();
     cy.get('[data-testid="sign-in-button"]').should('exist');
+    cy.get('[data-testid="account-button"]').click();
   })
   
 });
