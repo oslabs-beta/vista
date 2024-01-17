df00a57d1dd3e063fa0fcccd164f57e54f15ef97
Lucas Contreras <32287834+lucascontreras@users.noreply.github.com>
2024-01-17 14:58:39 -0300

    login is executed before each test

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,25 +3,13 @@ const apiUrl = "https://rickandmortyapi.com/graphql";
 // This code will run once before each test in the describe block
 beforeEach(() => {
   cy.visit("/").then(() => {
+    cy.login("steph@dangernoodles.com", "123456789");
     cy.get('input[name="endpoint-url"]').type(apiUrl);
     cy.get('[data-testid="endpoint-form"]').submit();
   });
 });
 
 describe("e2e Test for Vista", () => {
-  // Have user successfully sign in with their own credentials
-  it("Should successfully log in the user to the application using their own credentials", () => {
-    cy.get('[data-testid="account-button"]').should("exist");
-    cy.get('[data-testid="account-button"]').click();
-    cy.get('[data-testid="sign-in-button"]').should("exist");
-    cy.get('[data-testid="sign-in-button"]').click();
-    cy.get('[data-testid="username-input"]').should("exist");
-    cy.get('[data-testid="username-input"]').type("steph@dangernoodles.com");
-    cy.get('[data-testid="password-input"]').should("exist");
-    cy.get('[data-testid="password-input"]').type("123456789");
-    cy.contains("Sign in with Credentials").click();
-    cy.get('[data-testid="endpoint-form"]').should("exist");
-  });
 
   it("Should render a root and fields node on the DOM", () => {
     // Check to see that the nodes with the data-testid attributes have rendered
@@ -33,7 +21,7 @@ describe("e2e Test for Vista", () => {
   });
 
   // Check that the field(s) clicked on render in the query generator correctly
-  it("Should update the query generator based on the fields clicked and run the query", () => {
+  it("Should update the query generator based on the fields clicked and run the query", () => {    
     
     cy.get('[data-testid="query-generator"]').should("exist");
 
@@ -62,9 +50,6 @@ describe("e2e Test for Vista", () => {
 
   // Have user successfully sign out
   it("Should successfully log out the user to the application using their own credentials", () => {
-    // custom command
-    cy.login("steph@dangernoodles.com", "123456789");
-
     // logout
     cy.get('[data-testid="account-button"]').should("exist");
     cy.get('[data-testid="account-button"]').click();
